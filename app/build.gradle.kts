c4109c112b291e4db806fc97a5bf729037e2dbf8
testdoliet <contagamer5903@gmail.com>
2026-03-17 13:29:48 -0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -78,7 +78,7 @@ android {
 
     buildTypes {
         debug {
-            // signingConfig = signingConfigs.getByName("release") //
+            signingConfig = signingConfigs.getByName("debug") 
             isDebuggable = true
             isMinifyEnabled = false
 
